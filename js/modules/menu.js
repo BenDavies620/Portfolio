e1855a9354f4fc96f6ab8bb939b731495c538e90
BenDavies620 <benjamin.davies@netmatters-scs.com>
2026-09-24 09:21:10 +0100

    Made clicking off the sidebar and hitting escape close the sidebar

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -10,3 +10,21 @@ burger.addEventListener('click', () => {
         burger.classList.remove('open');
     }
 });
+
+document.addEventListener('click', (event) => {
+    if (
+        sidebar.classList.contains('open') &&
+        !sidebar.contains(event.target) &&
+        !burger.contains(event.target)
+    ) {
+        sidebar.classList.remove('open');
+        burger.classList.remove('open');
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && sidebar.classList.contains('open')) {
+        sidebar.classList.remove('open');
+        burger.classList.remove('open');
+    }
+});
